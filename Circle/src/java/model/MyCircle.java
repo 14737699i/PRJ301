f07d8eefa1a179e59822e7ce7486e5e42acba9c7
huyhoang <14737699i@gmail.com>
2022-05-18 11:39:08 +0700

    do request scope for Circle

---
+++ Circle/src/java/model/MyCircle.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author win
+ */
+public class MyCircle {
+    private int radius, x,y;
+    private String color;
+
+    public MyCircle() {
+    }
+
+    public MyCircle(int radius, int x, int y, String color) {
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+    
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
--- Circle/src/java/view/ViewCircles.java
+++ Circle/src/java/view/ViewCircles.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
+package view;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -11,20 +13,17 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.Random;
 import model.MyCircle;
 
 /**
  *
- * @author 14737
+ * @author win
  */
-@WebServlet(urlPatterns = {"/circle"})
-public class NewServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+@WebServlet(name="ViewCircles", urlPatterns={"/view/circle"})
+public class ViewCircles extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -38,18 +37,18 @@ public class NewServlet extends HttpServlet {
 //            out.println("<!DOCTYPE html>");
 //            out.println("<html>");
 //            out.println("<head>");
-//            out.println("<title>Servlet NewServlet</title>");  
+//            out.println("<title>Servlet ViewCircles</title>");  
 //            out.println("</head>");
 //            out.println("<body>");
-//            out.println("<h1>Servlet NewServlet at " + request.getContextPath () + "</h1>");
+//            out.println("<h1>Servlet ViewCircles at " + request.getContextPath () + "</h1>");
 //            out.println("</body>");
 //            out.println("</html>");
 //        }
 //    } 
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -57,13 +56,12 @@ public class NewServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-//        processRequest(request, response);
-    }
+    throws ServletException, IOException {
+       
+    } 
 
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -71,43 +69,37 @@ public class NewServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-//        processRequest(request, response);
-        String value = request.getParameter("value");
+    throws ServletException, IOException {
+        ArrayList<MyCircle> circles = (ArrayList<MyCircle>) request.getAttribute("circles");
         
-        int num = Integer.parseInt(value);
+        response.getWriter().println("<!DOCTYPE html>");
+        response.getWriter().println("<html>");
+        response.getWriter().println("<body>");
+        response.getWriter().println("<canvas id=\"myCanvas\" width=\"1000\" height=\"1000\" style=\"border:1px solid #d3d3d3;\">\n"
+                + "Your browser does not support the HTML5 canvas tag.</canvas>");
+        response.getWriter().println("<script>");
+        response.getWriter().println("var c = document.getElementById(\"myCanvas\");");
+        response.getWriter().println("var ctx = c.getContext(\"2d\");");
         
-        ArrayList<MyCircle> circles = new ArrayList<>();
-        
-        Random rand = new Random();
-        
-        final String colorString = "0123456789abcdef";
-        
-        String randomHexColor = "";
-        for (int i = 0; i < num; i++) {
-            MyCircle c = new MyCircle();
-            
-            c.setRadius(rand.nextInt(81) + 20);
-            c.setX(rand.nextInt(1000 - c.getRadius() * 2) + c.getRadius());
-            c.setY(rand.nextInt(1000 - c.getRadius() * 2) + c.getRadius());
-            for (int j = 0; j < 6; j++) {
-                randomHexColor = randomHexColor + colorString.charAt(rand.nextInt(16));
-            }
-            c.setColor(randomHexColor);
-            circles.add(c); 
-//            var randomColor = Double.toHexString(Math.floor(Math.random()*16777216))
-                    //            Integer.toHexString(rand.nextInt(16777216))
-            
-            randomHexColor = "";
-            
+        for (MyCircle c: circles){
+            response.getWriter().println("ctx.beginPath();");
+        String strockStyle = "ctx.strokeStyle = '#" + c.getColor() + "';";
+        String arc = "ctx.arc(" + c.getX() + ", " + c.getY() + ", " + c.getRadius() + ", 0, 2 * Math.PI);";
+        response.getWriter().println(arc);
+        response.getWriter().println("ctx.lineWidth = 4;");
+        response.getWriter().println(strockStyle);
+        response.getWriter().println("ctx.stroke();");
         }
-        request.setAttribute("circles", circles);
-        request.getRequestDispatcher("view/circle").forward(request, response);
+        
+        
+        
+        response.getWriter().println("</script>");
+        response.getWriter().println("</body>");
+        response.getWriter().println("</html>");
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override
