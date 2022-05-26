98c7fe435a4aceb8cf848cdc5626f183bc44791a
huyhoang <14737699i@gmail.com>
2022-05-26 23:27:41 +0700

    buildding Login project

---
+++ LoginExercise/src/java/dal/AccountDBContext.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import com.oracle.wls.shaded.org.apache.bcel.generic.AALOAD;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Account;
+
+/**
+ *
+ * @author win
+ */
+public class AccountDBContext extends DBContext {
+
+    public ArrayList<Account> getAccount() {
+        ArrayList<Account> accounts = new ArrayList<>();
+        try {
+            String sql = "select username, password, displayName from Account";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Account a = new Account();
+                a.setUsername(rs.getString(0));
+                a.setPassword(rs.getString(1));
+                a.setPassword(rs.getString(2));
+                accounts.add(a);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return accounts;
+    }
+}
