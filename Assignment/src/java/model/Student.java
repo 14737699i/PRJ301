4f249ceb83f71aa489043c660b3803a978a8c025
huyhoang <14737699i@gmail.com>
2022-06-15 01:48:25 +0700

    mockup

--- Assignment/src/java/model/Student.java
+++ Assignment/src/java/model/Student.java
@@ -4,9 +4,49 @@
  */
 package model;
 
+import java.sql.Date;
+
 /**
  *
  * @author win
  */
 public class Student {
+    private int id;
+    private String name;
+    private Date dob;
+    private boolean gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+    
+    
 }
