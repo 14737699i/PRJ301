1e9e6793a71afb6f23341726d4e00b7f192f261f
huyhoang <14737699i@gmail.com>
2022-06-22 01:18:35 +0700

    Update AttendanceReport.java

--- Assignment/src/java/model/AttendanceReport.java
+++ Assignment/src/java/model/AttendanceReport.java
@@ -4,10 +4,69 @@
  */
 package model;
 
+import java.sql.Date;
+
 /**
  *
  * @author win
  */
 public class AttendanceReport {
+    private int id;
+    private int sessionId;
+    private Student student;
+    private boolean status;
+    private String comment;
+    private Date recordTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(int sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Date getRecordTime() {
+        return recordTime;
+    }
+
+    public void setRecordTime(Date recordTime) {
+        this.recordTime = recordTime;
+    }
+    
+    
+    
     
 }
