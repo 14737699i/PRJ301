ee8099739874ff0da3418f5568b46020a4f9e5a3
huyhoang <14737699i@gmail.com>
2022-06-22 00:15:43 +0700

    Update Session.java

--- Assignment/src/java/model/Session.java
+++ Assignment/src/java/model/Session.java
@@ -9,5 +9,69 @@ package model;
  * @author win
  */
 public class Session {
+    private int id;
+    private String groupId;
+    private int timeSlotId;
+    private int sessionNo;
+    private int sessionDate;
+    private Room room;
+    private Lecture lecture;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public int getTimeSlotId() {
+        return timeSlotId;
+    }
+
+    public void setTimeSlotId(int timeSlotId) {
+        this.timeSlotId = timeSlotId;
+    }
+
+    public int getSessionNo() {
+        return sessionNo;
+    }
+
+    public void setSessionNo(int sessionNo) {
+        this.sessionNo = sessionNo;
+    }
+
+    public int getSessionDate() {
+        return sessionDate;
+    }
+
+    public void setSessionDate(int sessionDate) {
+        this.sessionDate = sessionDate;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Lecture getLecture() {
+        return lecture;
+    }
+
+    public void setLecture(Lecture lecture) {
+        this.lecture = lecture;
+    }
+    
     
 }
